ff978a86c4cebbf4f95bb3d440547b132cba88dc
Hrithik Sagar <hrithiksagar36@gmail.com>
2022-01-17 17:18:51 +0530

    codewriting code

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@
 d3.json("data/data.json").then(function(data){
 	console.log(data);
 })
+// writing code here:
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
+
+const svg = d3.select("#chart-area").append("svg").attr("width",  WIDTH+MARGIN.LEFT + MARGIN.RIGHT).attr("height",HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+
+const g = svg.append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+console.log("1")
+console.log(g)
+
+
+let time = 0
+console.log("2")
+console.log(time) // should be o
+
+
+// scales:
+const x = d3.scaleLog().base(10).range([0,WIDTH]).domain([142,150000])
+console.log("3")
+console.log(x)
+
+
